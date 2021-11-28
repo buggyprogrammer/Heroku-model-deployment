cd9afb7dcf1bbab49e2ca0fd4f9ea95564722f9b
DarkStar <darkstardreamisp@gmail.com>
2021-11-28 15:08:48 +0530

    successful model deployment

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+# import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pik', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        age = request.form['age']
+        exp = request.form['exp']
+
+    input_arr = [[age, exp]]
+    prediction = model.predict(input_arr)
+    return render_template('index.html', output=f'Your expected salary should be $ {float(prediction):0.2f}')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
